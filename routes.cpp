b9eb3ff28e3dd15ff31c6a4387f9d9902afac495
Vismaya3 <vismayaramesh3@gmail.com>
2023-08-22 20:31:08 +0530

    first cpp commit

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,23 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    int result =input["first"].i()+input["second"].i();
+    crow::json::wvalue responseJson;
+    responseJson["result"] = result;
+    return crow::response{responseJson};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    int first = input["first"].i();
+    int second = input["second"].i();
+    int result = first-second;
+    crow::json::wvalue responseJson;
+    responseJson["result"] = result;
+    return crow::response{responseJson};
 }
